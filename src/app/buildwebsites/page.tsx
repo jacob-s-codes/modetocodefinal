9a8f11e687805f22f3289107fa55db7b7e1d8a9e
jacob-s-codes <jacobbshaul@gmail.com>
2025-04-26 18:35:21 -0700

    Added and updated build websites

--- src/app/buildwebsites/page.tsx
+++ src/app/buildwebsites/page.tsx
@@ -15,9 +15,9 @@ const page = () => {
 
                 <CardRight title={"Step 2: The Odin Project"} description={'Another completely free resource that allows you to learn the basics of HTML and CSS but also more advanced topics if you\'re interested.'} imageUrl={'/theodinprojectimage.png'} buttonText={'Try it Out!'} buttonUrl={'https://www.theodinproject.com/paths/foundations/courses/foundations#html-foundations'} ></CardRight>
 
-                <CardLeft title={'Step 3: Free Code Camp AI Course'} description={'This almost 4-hour video will help you create your own AI self-driving car. This is a great way to learn how to code AI projects.'} imageUrl={'/fccai.png'} buttonText={'Watch Now!'} buttonUrl={'https://www.youtube.com/watch?v=VgzHT9quo5c'}></CardLeft>
+                <CardLeft title={'Step 3: Free Code Camp HTML and CSS'} description={'This website provides fantastic, free resources to help you learn almost anything about coding that you would want, but their web development course specifically is incredibly helpful!'} imageUrl={'/freecodecamplogo.png'} buttonText={'Try it Out!'} buttonUrl={'https://www.freecodecamp.org/'}></CardLeft>
 
-                <CardRight title={"Step 4: Google\'s AI Course"} description={'Try out Google\'s own free AI course, \"AI for Anyone\". This fantastic course is taught by Google\'s AI experts and will teach you the basics of AI.'} imageUrl={'/googleimg.jpeg'} buttonText={'Try it out!'} buttonUrl={'https://www.edx.org/learn/machine-learning/harvard-university-machine-learning-and-ai-with-python'} ></CardRight>
+                <CardRight title={"Step 4: Tailwind CSS"} description={'Once you\'ve got the basics of HTML and CSS, you can start customizing and styling your websites to look cool and modern. I used Tailwind CSS to make this website and I highly recommend it! Here\'s a free 4 hour course that will teach you everything you need!'} imageUrl={'/tailwindlogo.webp'} buttonText={'Try it out!'} buttonUrl={'https://www.youtube.com/watch?v=ft30zcMlFao'} ></CardRight>
             </div>
 
         </div>
