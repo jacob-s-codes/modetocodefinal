9005f697918be589f1bc7200c17d16b949d86968
jacob-s-codes <jacobbshaul@gmail.com>
2025-05-17 11:38:58 -0700

    Added rowan picture and text

--- src/app/meettheteam/page.tsx
+++ src/app/meettheteam/page.tsx
@@ -10,9 +10,23 @@ const people = [
             "I founded Mode to Code over the summer of 2024, and am passionate about teaching technology. I enjoy coding, playing drums, and hanging out with my family. You can email me at: jacob@modetocode.com",
     },
     {
+        image: "/teachingimg2.jpg",
+        name: "Aydin K",
+        title: "Assisted Living Program Co-Lead",
+        description:
+            "Passionate about teaching and empowering young minds with tech skills.",
+    },
+    {
+        image: "/teachingimg2.jpg",
+        name: "Graham T",
+        title: "Assisted Living Program Co-Lead",
+        description:
+            "Passionate about teaching and empowering young minds with tech skills.",
+    },
+    {
         image: "/rohanpic.jpg",
         name: "Rohan S",
-        title: "Instructor and Hackathon Organizer",
+        title: "Instructor and Head Code Support Volunteer for UHS Hacks",
         description:
             "I'm a teacher with Mode to Code, and I’ve loved coding since I was in elementary school. I now like working on small coding projects, usually websites. I also enjoy playing piano and tennis.",
     },
@@ -37,20 +51,7 @@ const people = [
         description:
             "Passionate about teaching and empowering young minds with tech skills.",
     },
-    {
-        image: "/teachingimg2.jpg",
-        name: "Aydin K",
-        title: "Assisted Living Program Co-Lead",
-        description:
-            "Passionate about teaching and empowering young minds with tech skills.",
-    },
-    {
-        image: "/teachingimg2.jpg",
-        name: "Graham T",
-        title: "Assisted Living Program Co-Lead",
-        description:
-            "Passionate about teaching and empowering young minds with tech skills.",
-    },
+    
     {
         image: "/ellapic.jpg",
         name: "Ella S",
@@ -66,11 +67,11 @@ const people = [
             "Passionate about teaching and empowering young minds with tech skills.",
     },
     {
-        image: "/teachingimg2.jpg",
+        image: "/rowanpic.jpeg",
         name: "Rowan C",
         title: "Hackathon Lead Outreach",
         description:
-            "Passionate about teaching and empowering young minds with tech skills.",
+            "Hi, I’m Rowan, a junior at San Francisco University High School. I joined UHS Hacks because I want to help create a space where students from across the Bay Area can come together to learn, build, and collaborate. I’ve seen how empowering it can be to bring an idea to life with code, and I wanted to be part of making that opportunity accessible to everyone. Outside of school, I enjoy biking, exploring nature, and traveling. I also work at a pizza restaurant and have lived in London, which has given me a love for exploring diverse cultures and experiences. You can email me at: Rowan.chandra_26@sfuhs.org",
     },
     {
         image: "/teachingimg2.jpg",
