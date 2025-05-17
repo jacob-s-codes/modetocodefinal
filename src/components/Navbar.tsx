74ed6490de0fdccf9aa39eec67027fa8ebc2c9e2
jacob-s-codes <jacobbshaul@gmail.com>
2025-05-16 21:38:27 -0700

    removed test useEffect

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,9 +22,6 @@ const Navbar = () => {
         };
     }, [isMobileMenuOpen]);
 
-    useEffect(() => {
-        console.log("Hello");
-    });
 
     const toggleMobileMenu = () => {
         setIsMobileMenuOpen(!isMobileMenuOpen);
