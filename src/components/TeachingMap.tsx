853b70953a60bc9567414c496e1853b1673c28bf
jacob-s-codes <jacobbshaul@gmail.com>
2025-05-23 16:48:27 -0700

    Added boundaries

--- src/components/TeachingMap.tsx
+++ src/components/TeachingMap.tsx
@@ -14,7 +14,11 @@ const DefaultIcon = L.icon({
 
 export default function TeachingMap() {
     return (
-        <MapContainer center={[51.505, 2]} zoom={2} scrollWheelZoom={false} dragging={false} zoomControl={false} doubleClickZoom={false} keyboard={false} className="w-full rounded-xl z-0" style={{ height: "calc(100vh - 200px)" }}>
+        <MapContainer center={[51.505, 2]} zoom={2} scrollWheelZoom={false} worldCopyJump={false} maxBounds={[
+            [-85, -180], // Southwest corner
+            [85, 180],   // Northeast corner
+          ]}
+          maxBoundsViscosity={1.0} dragging={true} zoomControl={false} doubleClickZoom={false} keyboard={false} className="w-full rounded-xl z-0" style={{ height: "calc(100vh - 200px)" }}>
             <TileLayer
                 attribution='&copy; <a href="https://carto.com/">CARTO</a>'
   url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
