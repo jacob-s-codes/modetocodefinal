4aabdd83aea052aea08b54a36c654e6163abd4b0
jacob-s-codes <jacobbshaul@gmail.com>
2025-05-24 11:13:05 -0700

    Added not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <div className="h-screen text-center flex flex-col items-center justify-center gap-y-4">
+            <h1 className="text-4xl font-bold max-w-xl px-2">The page you're looking for can't be found.</h1>
+            <p className="text-lg mt-4">If you think this is a mistake, please contact us at <a href="mailto:info@modetocode.com" className="text-blue-600 underline">info@modetocode.com</a>.</p>
+        </div>
+    );
+}
