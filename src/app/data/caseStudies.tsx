541b2a6741eb153da655f530abc55457eb07b83a
jacob-s-codes <jacobbshaul@gmail.com>
2025-06-06 10:56:44 -0700

    updated case studies

--- src/app/data/caseStudies.tsx
+++ src/app/data/caseStudies.tsx
@@ -7,7 +7,7 @@ const caseStudies = [
         image1: "/teachingimg17.jpg",
         image2: "/teachingimg16.jpg",
         image3: "/teachingimg5.jpg",
-        description1: "In January 2025, Mode to Code began teaching classes at CAIS, a private school in San Francisco. One of the first classes in our program, this class helped solidify our curriculum and teaching methods. This course was taught by a host of Mode to Code instructors, including Jacob, Rohan, Caroline, and Ella. The class was a great success, with students learning the basics of HTML, CSS, and JavaScript. The students were engaged and excited to learn, and many expressed interest in continuing their coding education.",
+        description1: "In January 2025, Mode to Code began teaching classes at CAIS, a private school in San Francisco. One of the first classes in our program, it helped solidify our curriculum and teaching methods. This course was taught by a host of Mode to Code instructors, including Jacob, Rohan, Caroline, and Ella. The class was a great success, with students learning the basics of HTML, CSS, and Javascript. The students were engaged and excited to learn, and many expressed interest in continuing their coding education.",
         description2: "The students were able to create their own websites using the skills they learned in class. They were also able to collaborate with each other and share their work. The class was a great success, and we are excited to continue working with CAIS in the future.",
         website: "https://www.cais.org/",
         share: "modetocode.com/casestudies/cais",
