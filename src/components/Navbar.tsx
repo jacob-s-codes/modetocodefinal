cfe968abbbf2529a5f99a7a9c4d5d75b59767d47
jacob-s-codes <jacobbshaul@gmail.com>
2025-06-11 22:01:26 -0700

    added font bold

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -92,7 +92,7 @@ const Navbar = () => {
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-file-person-fill h-8 mb-3 mr-3 fill-current text-white" viewBox="0 0 16 16">
                                                     <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2m-1 7a3 3 0 1 1-6 0 3 3 0 0 1 6 0m-3 4c2.623 0 4.146.826 5 1.755V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1v-1.245C3.854 11.825 5.377 11 8 11" />
                                                 </svg>
-                                                <h3 className="addfont-bold text-xl text-white uppercase mb-2">Meet the team</h3>
+                                                <h3 className="font-bold text-xl text-white uppercase mb-2">Meet the team</h3>
                                             </div>
                                             <p className="text-sm text-white">Check out the amazing volunteers, teachers and leaders who help Mode to Code</p>
                                             <div onClick={handleMegaMenuLinkClick} className='w-fit'>
