0011b1f646a929d5e65d63f16d929bc94329f90d
jacob-s-codes <jacobbshaul@gmail.com>
2025-07-16 10:04:54 -0700

    added links to carosel

--- src/components/Carosel.tsx
+++ src/components/Carosel.tsx
@@ -7,22 +7,22 @@ export default function App() {
     <>
       <section className="flex justify-center items-center ">
         <div className=" h-[200px] rounded-2xl w-8xl">
-          <Marquee className="  py-3 overflow-hidden grid  " speed={150}>
-            <ImageforCarosel src="/breakthrough.png"/>
-            <ImageforCarosel src="/bolivia/aisblogowide.png"/>
-            <ImageforCarosel src="/liveoaklogo.jpg"/>
-            <ImageforCarosel src="/caislogo.jpg"/>
-            <ImageforCarosel src="/assistedliving/ivyparklogo.webp"/>
-            <ImageforCarosel src="/rhodapic.jpg"/>
-            <ImageforCarosel src="/botswana/westwoodlogo.png"/>
-            <ImageforCarosel src="/assistedliving/franklogo.webp"/>
-            <ImageforCarosel src="/schools/isflorence.png"/>
-            <ImageforCarosel src="/sfpllogo.png"/>
-            <ImageforCarosel src="/schools/presidiomiddlelogo.jpg"/>
-            <ImageforCarosel src="/schools/neilcumminslogo.png"/>
-            <ImageforCarosel src="/assistedliving/coterielogo.png"/>
-            <ImageforCarosel src="/schools/delhilogo.jpg"/>
-            <ImageforCarosel src="/kismetlogo.png"/>
+          <Marquee className="  py-3 overflow-hidden grid  " speed={150} pauseOnHover={true}>
+            <ImageforCarosel src="/breakthrough.png" href="https://www.breakthroughsf.org/"/>
+            <ImageforCarosel src="/bolivia/aisblogowide.png" href="https://www.aisb.edu.bo/eng.html#"/>
+            <ImageforCarosel src="/liveoaklogo.jpg" href="https://www.liveoaksf.org/"/>
+            <ImageforCarosel src="/caislogo.jpg" href="https://www.cais.org/"/>
+            <ImageforCarosel src="/assistedliving/ivyparklogo.webp" href="https://ivyliving.com/cathedralhill/"/>
+            <ImageforCarosel src="/rhodapic.jpg" href="https://rgplaza.jfcs.org/"/>
+            <ImageforCarosel src="/botswana/westwoodlogo.png" href="https://www.westwood.ac.bw/"/>
+            <ImageforCarosel src="/assistedliving/franklogo.webp" href="https://frankresidences.org/"/>
+            <ImageforCarosel src="/schools/isflorence.png" href="https://www.isfitaly.org/"/>
+            <ImageforCarosel src="/sfpllogo.png" href="https://sfpl.org/locations/noe-valley"/>
+            <ImageforCarosel src="/schools/presidiomiddlelogo.jpg" href="https://www.sfusd.edu/school/presidio-middle-school"/>
+            <ImageforCarosel src="/schools/neilcumminslogo.png" href="https://www.lcmschools.org/en-US"/>
+            <ImageforCarosel src="/assistedliving/coterielogo.png" href="https://www.coterieseniorliving.com/luxury-retirement-communities/coterie-cathedral-hill-san-francisco-ca"/>
+            <ImageforCarosel src="/schools/delhilogo.jpg" href="https://www.facebook.com/ISEdelhinangloi/"/>
+            <ImageforCarosel src="/kismetlogo.png" href="https://sites.google.com/shcp.edu/kismet/home?authuser=1"/>
           </Marquee>
         </div>
       </section>
--- src/components/ImageforCarosel.tsx
+++ src/components/ImageforCarosel.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
 interface BasicButtonProps {
-    src: string
+    src: string,
+    href: string
 }
 
-const BasicButton = ({ src }: BasicButtonProps) => {
+const BasicButton = ({ src, href }: BasicButtonProps) => {
     return (
-        <div className=" flex lg:pr-10 pr-6 flex-col justify-center items-center h-[350px] mx-5 ">
-            <img src={src} className="lg:w-64 md:w-32 w-28" alt="" />
+        <div className=" flex lg:mx-10 mx-6 flex-col justify-center items-center h-[350px] ">
+            <a href={href} target='_blank'><img src={src} className="lg:w-64 md:w-32 w-28" alt="" /></a>
         </div>
     )
 }
