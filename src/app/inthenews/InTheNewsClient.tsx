27406d8a015d1097ab5b0e6c53f25d5b3605ae64
jacob-s-codes <jacobbshaul@gmail.com>
2025-08-01 11:17:20 -0700

    added shelburne news article

--- src/app/inthenews/InTheNewsClient.tsx
+++ src/app/inthenews/InTheNewsClient.tsx
@@ -88,8 +88,26 @@ export default function InTheNews() {
                         </div>
                     </div>
 
+                    <div className="flex lg:flex-row flex-col-reverse parent-leftright lg:items-center justify-between">
+                        <div className="flex flex-col xl:max-w-2xl max-w-xl animateleft">
+                            <h3 className="uppercase text-blue-900 lg:text-3xl text-2xl">in the...</h3>
+                            <h2 className="uppercase lg:text-4xl text-3xl font-bold pb-2">Shelburne News</h2>
+                            <hr className="border-4 border-black mb-8" />
+                            <p className="lg:text-2xl text-xl font-light mb-8">The Mode to Code afterschool class at Shelburne Community School was the first class Jacob Shaul ever taught online. Now, he&apos;s going to be teaching classes around the world...</p>
+                            <Link href="https://www.vtcng.com/shelburnenews/news/local_news/mode-to-code-goes-from-shelburne-to-botswana-and-beyond/article_f62633bd-b646-4dd0-9eef-adffee739fe3.html" target="_blank" className="relative inline-flex px-8 py-4 font-medium group w-fit">
+                                <span
+                                    className="absolute inset-0 transition duration-500 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:translate-x-0 group-hover:translate-y-0"></span>
+                                <span className="absolute inset-0 bg-white border-2 border-black group-hover:bg-black"></span>
+                                <span className="relative text-black group-hover:text-white uppercase">READ MORE</span>
+                            </Link>
+                        </div>
+                        <div className="max-w-3xl relative animateright rounded-xl lg:ml-8 lg:pb-0 pb-8">
+                            <img src="/news/shelburne.png" alt="" className="w-full rounded-xl shadow-2xl shadow-darkbg" />
+                        </div>
+                    </div>
 
-                    
+
+
                 </div>
 
 
