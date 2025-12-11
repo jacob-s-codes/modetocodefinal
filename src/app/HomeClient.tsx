fbd6a2de74c5c80f3a0bf74cb0f2b21e55ade366
jacob-s-codes <jacobbshaul@gmail.com>
2025-12-10 18:34:34 -0800

    changed button to see instead of read

--- src/app/HomeClient.tsx
+++ src/app/HomeClient.tsx
@@ -119,7 +119,7 @@ export default function HomeClient() {
                                 <span
                                     className="absolute inset-0 transition duration-500 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:translate-x-0 group-hover:translate-y-0"></span>
                                 <span className="absolute inset-0 bg-white border-2 border-black group-hover:bg-black"></span>
-                                <span className="relative text-black group-hover:text-white uppercase">Read more features</span>
+                                <span className="relative text-black group-hover:text-white uppercase">See more features</span>
                             </Link>
                         </div>
 
