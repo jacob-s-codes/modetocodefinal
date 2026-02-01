905208a03d693e725eb639279276cfae10681684
jacob-s-codes <jacobbshaul@gmail.com>
2026-01-31 22:20:21 -0800

    updated background

--- src/app/inthenews/InTheNewsClient.tsx
+++ src/app/inthenews/InTheNewsClient.tsx
@@ -12,9 +12,9 @@ export default function InTheNews() {
         <div className="w-full overflow-x-hidden overflow-y-hidden">
             <div className="inthenewsbg h-screen w-full bg-cover bg-center overflow-y-hidden">
                 <div className="h-full flex flex-col justify-center items-center text-white">
-                    <h1 className="lg:text-9xl text-7xl font-black text-center text-transparent bg-clip-text bg-gradient-to-r to-blue-200 from-blue-500">
+                    {/* <h1 className="lg:text-9xl text-7xl font-black text-center text-transparent bg-clip-text bg-gradient-to-r to-blue-200 from-blue-500">
                         MODE TO CODE
-                    </h1>
+                    </h1> */}
                     <h1 className="lg:text-9xl text-7xl font-black text-center text-transparent bg-clip-text bg-gradient-to-r to-blue-200 from-blue-500">
                         IN THE NEWS
                     </h1>
