5bea910876fa8ec77900398d83557e574c3be579
jacob-s-codes <jacobbshaul@gmail.com>
2026-03-14 18:43:30 -0700

    added live oak images

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -159,7 +159,7 @@ const Navbar = () => {
 
                                     {/* Image Section - 50% width */}
                                     <div className="w-full lg:w-1/2 px-4">
-                                        <img src="/teachingimg29.jpg" alt="MTC Logo" className="w-full h-auto object-contain rounded-lg lg:flex hidden" />
+                                        <img src="/schools/liveoakchangemaker2.jpg" alt="MTC Logo" className="w-full h-auto object-contain rounded-lg lg:flex hidden" />
                                     </div>
                                 </div>
                             </div>
