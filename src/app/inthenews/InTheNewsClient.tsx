498bfe33152a046267e08c3ab937bfb344e57fc2
jacob-s-codes <jacobbshaul@gmail.com>
2026-04-23 19:48:15 -0700

    added 74 million

--- src/app/inthenews/InTheNewsClient.tsx
+++ src/app/inthenews/InTheNewsClient.tsx
@@ -71,6 +71,18 @@ export default function InTheNews() {
                         
                     />
 
+                     <NewsSection
+                        subtitle="On..."
+                        title="The 74 Million"
+                        excerpt={`"As a sophomore in high school, Jacob Shaul started Mode to Code to teach programming and web development to other kids..."`}
+                        link="https://www.the74million.org/article/this-high-school-student-is-teaching-kids-across-the-globe-how-to-code/"
+                        imgSrc="/news/74million.png"
+                        buttontext="Read more"
+                        reverse 
+                    />
+
+                    
+
                     <NewsSection
                         subtitle="On..."
                         title="The San Francisco Experience Podcast"
@@ -78,7 +90,7 @@ export default function InTheNews() {
                         link="https://www.thesanfranciscoexperiencepodcast.com/mode-to-code-educating-students-of-all-ages-around-the-world-about-coding-and-technology-talking-w/"
                         imgSrc="/news/sfexperiencepod.png"
                         buttontext="Listen Now"
-                        reverse
+                        
                     />
 
                     <NewsSection
@@ -88,7 +100,7 @@ export default function InTheNews() {
                         link="https://podcasts.apple.com/us/podcast/from-school-club-to-global-classroom-jacob-shauls-mode/id1773055733?i=1000738765740"
                         imgSrc="/news/launchx.jpeg"
                         buttontext="Listen Now"
-                        
+                        reverse
                     />
 
                     <NewsSection
@@ -97,7 +109,7 @@ export default function InTheNews() {
                         excerpt={`"Healing the world wasn’t Jacob Shaul’s primary goal in 2024 when, as a high school sophomore, he started teaching computer coding to students at a local middle school..."`}
                         link="https://jweekly.com/2026/03/05/teens-volunteer-run-project-teaches-coding-to-younger-kids-and-seniors-too/"
                         imgSrc="/news/thejfeature.png"
-                        reverse
+                        
                     />
 
                     <NewsSection
@@ -106,7 +118,7 @@ export default function InTheNews() {
                         excerpt={`"Jacob Shaul is thankful to have learned how to code in middle school and wants to give the same opportunity to others..."`}
                         link="https://patch.com/california/san-francisco/bay-area-student-gives-back-community-mode-code-program"
                         imgSrc="/news/patchimg.png"
-                        
+                        reverse
                     />
 
 
@@ -116,7 +128,7 @@ export default function InTheNews() {
                         excerpt={`"We caught up with the brilliant and insightful Jacob Shaul a few weeks ago and have shared our conversation below..."`}
                         link="https://canvasrebel.com/meet-jacob-shaul/"
                         imgSrc="/news/canvasrebel.png"
-                        reverse
+                        
                     />
 
                     <NewsSection
@@ -125,7 +137,7 @@ export default function InTheNews() {
                         excerpt={`"The Young Alumni Changemaker Award began in 2022 to recognize that Live Oak students do not wait to make an impact in their communities. The award honors current high school juniors and seniors who graduated from Live Oak that model..."`}
                         link="https://www.liveoaksf.org/news-detail?pk=949124"
                         imgSrc="/news/liveoakaward.png"
-                        
+                        reverse
                     />
 
                     <NewsSection
@@ -134,7 +146,7 @@ export default function InTheNews() {
                         excerpt={`"We recently connected with Jacob Shaul and have shared our conversation below...."`}
                         link="https://boldjourney.com/meet-jacob-shaul/"
                         imgSrc="/news/boldjourney.png"
-                        reverse
+                        
                     />
 
                     <NewsSection
@@ -143,7 +155,7 @@ export default function InTheNews() {
                         excerpt={`"Glen Park's Jacob Shaul is making computer coding accessible for all ages. The San Francisco University High School junior spent the last year, with help from UHS' Dean of Teaching & Learning Byron Philhour..."`}
                         link="https://www.inglesidelight.com/mode-to-code-jacob-shaul/"
                         imgSrc="/news/inglesidelight.jpg"
-                        
+                        reverse
                     />
 
                     <NewsSection
@@ -152,7 +164,7 @@ export default function InTheNews() {
                         excerpt={`"The Mode to Code afterschool class at Shelburne Community School was the first class Jacob Shaul ever taught online. Now, he's going to be teaching classes around the world..."`}
                         link="https://www.vtcng.com/shelburnenews/news/local_news/mode-to-code-goes-from-shelburne-to-botswana-and-beyond/article_f62633bd-b646-4dd0-9eef-adffee739fe3.html"
                         imgSrc="/news/shelburneimg.png"
-                        reverse
+                        
                     />
 
                     <NewsSection
@@ -161,7 +173,7 @@ export default function InTheNews() {
                         excerpt={`"For Jacob Shaul, technology has always been more than a subject - it's a way to connect and create. His interest in it began in middle school and deepened at UHS, where he..."`}
                         link="https://issuu.com/sfuhsorg/docs/uhs_journal_fall_2025/21"
                         imgSrc="/news/UHSjournal.png"
-                        
+                        reverse
                     />
                 </div>
             </div>
